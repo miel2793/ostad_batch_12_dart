af569b93ac8c7be2088d48676f011b971696de4d
miel2793 <mahmudsifat2793@gmail.com>
2025-07-05 11:35:52 +0600

    new commit added

--- bin/Variable/map.dart
+++ bin/Variable/map.dart
@@ -2,5 +2,8 @@ main()
 {
   Map person ={ 'Name':'MD: Miel Mahmud','Age':22,'Location':'Mirpur'};
   person['code']=45;
+  person.addAll({'brand':'lk','brand code':859});
+  //person.clear({'brand'});
+  person.remove('Age');
   print(person);
 }
