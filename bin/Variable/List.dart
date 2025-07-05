65030b5c899d9fd21d010f7d2fdecb967cd62b94
miel2793 <mahmudsifat2793@gmail.com>
2025-07-05 10:37:15 +0600

    new commit added

--- bin/Variable/List.dart
+++ bin/Variable/List.dart
@@ -1,7 +1,19 @@
 main()
 {
+  //growable List e value add kora zay
+  // fixed List e value add kora zay na  datatype hisebe  final/constant use hoy
+
   var details =['miel','asif',0,'ovi'];
   print(details[0].runtimeType);
+  print(details.first);
 details.insert(0, 'misat');
 print(details);
+  print(details.length);
+
+  print(details.reversed);
+  print(details.isNotEmpty);
+
+
+
+
 }
