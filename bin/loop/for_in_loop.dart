10e9e63c7aa3141f708da5e6da034e141b05b191
miel2793 <mahmudsifat2793@gmail.com>
2025-07-06 12:18:31 +0600

    new commit added

--- bin/loop/for_in_loop.dart
+++ bin/loop/for_in_loop.dart
@@ -8,9 +8,18 @@ main()
 print(l[i]);
   }*/
   //set
-  var m ={2,3,4,5,6,7,8,9,0};
+  /*var m ={2,3,4,5,6,7,8,9,0};
   for(var i in m)
     {
 print(i);
+    }*/
+   //jeson array
+  var l=[{'Name':'miel',"ID":'368','Intake':'52'},
+    {'Name':'Siam',"ID":'878','Intake':'52'},
+    {'Name':'ovi',"ID":'398','Intake':'57'},
+    {'Name':'Anas',"ID":'364','Intake':'53'}];
+  for(var i in l)
+    {
+      print(i['Name']);
     }
 }
