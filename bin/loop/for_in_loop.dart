88a53ee0ab18d1b9e124dfbea78866ab5e36eda2
miel2793 <mahmudsifat2793@gmail.com>
2025-07-06 12:10:59 +0600

    new commit added

--- bin/loop/for_in_loop.dart
+++ bin/loop/for_in_loop.dart
@@ -1,9 +1,16 @@
 //loop_over_List
 main()
 {
-  var l=[3,3,5,6,7,8,9,3,4];
-  for(var i in l)
+  //List
+  /*var l=[3,3,5,6,7,8,9,3,4];
+  for(var i=0 ;i<=3;i++)
   {
+print(l[i]);
+  }*/
+  //set
+  var m ={2,3,4,5,6,7,8,9,0};
+  for(var i in m)
+    {
 print(i);
-  }
+    }
 }
