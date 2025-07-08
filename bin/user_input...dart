d73f70ca0e7c711ff9a6490fc3240ce8261ba552
miel2793 <mahmudsifat2793@gmail.com>
2025-07-08 22:39:20 +0600

    new commit added

--- bin/user_input...dart
+++ bin/user_input...dart
@@ -1,14 +1,9 @@
 
 import 'dart:io';
 
-main()
-{
-
-  int? name ;
+main() {
+  int? name;
   print("Enter your number  = ");
-  name=int.tryParse(stdin.readLineSync()!);
-  print('My number   is $name');
-//
-
-
+  name= int.tryParse(stdin.readLineSync()!);
+  print(name);
 }
