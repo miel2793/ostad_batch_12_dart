15417a8765ac3fd391e7d8880efda108fc0d3460
miel2793 <mahmudsifat2793@gmail.com>
2025-07-14 22:07:28 +0600

    new commit added

--- bin/Function/function_1.dart
+++ bin/Function/function_1.dart
@@ -1,16 +1,31 @@
- main() {
- print("the area 1 is ${cal(3, 8)}");
- print("the area 2  is ${calculatedArea(3, 86)}");
- print("the area 1 is ${cal(89, 8)}");
+import 'dart:io';
 
+main() {
+  int a = int.parse(stdin.readLineSync()!);
+  int b = int.parse(stdin.readLineSync()!);
 
-
- }
-
- calculatedArea(double length, double wide) {
-  double area = length * wide;
-  return area;
+  l(a, b);
 }
-//arrow Function 
-var cal=(double length ,double wide)=>(length*wide);
 
+sum(int a, int b) {
+  int sum;
+  sum = a + b;
+  print(sum);
+  return sum;
+}
+
+sub(int a, int b) {
+  int sub;
+  sub = a - b;
+  print(sub);
+  return sub;
+}
+
+l(int a, int b) {
+  if (a == b) {
+    print('we are same ');
+  } else {
+    print('we are not ');
+  }
+  //return l;
+}
