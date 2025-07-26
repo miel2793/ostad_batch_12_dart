c203551b32d622a816e08516857790ee953fcb8f
miel2793 <mahmudsifat2793@gmail.com>
2025-07-26 22:53:37 +0600

    new commit added

--- bin/loop/for_loop_at_list.dart
+++ bin/loop/for_loop_at_list.dart
@@ -2,17 +2,16 @@ import 'dart:io';
 
 main()
 {
-  print('how many Student  present  today: ');
-  int s = int.parse(stdin.readLineSync()!);
-  List<String> sname=[];
-  for(int i=0;i<s;i++)
-    {
-      String name=stdin.readLineSync()!;
-      sname.add(name);
-
-    }
-  for(int i=0;i<sname.length;i++)
-    {
-      print('your son  ${sname[i]} is present at today');
-    }
+  print("How Many Worker Is Absent  Today: ");
+   int worker=int.parse(stdin.readLineSync()!);
+   List<String>wName=[];
+   for(int i=0;i<worker;i++)
+     {
+       String name=stdin.readLineSync()!;
+       wName.add(name);
+     }
+   for(int i=0;i<wName.length;i++)
+     {
+       print("Dear ${wName[i]} . You Are Absent Today  Without any  Permission .\nSo we Decided To Cut Your Todays Payment\nThank You ");
+     }
 }
