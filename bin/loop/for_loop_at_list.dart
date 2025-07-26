9c5c6ce970c1aac3b5b28f1984ee5ba74b4741e9
miel2793 <mahmudsifat2793@gmail.com>
2025-07-26 22:46:38 +0600

    new commit added

--- bin/loop/for_loop_at_list.dart
+++ bin/loop/for_loop_at_list.dart
@@ -1,8 +1,18 @@
+import 'dart:io';
+
 main()
 {
-  List <String> student =['Rahim','Karim','ovi','robin','Sulaiman','Anas'];
-  for(int i=0;i<student.length;i++)
+  print('how many Student  present  today: ');
+  int s = int.parse(stdin.readLineSync()!);
+  List<String> sname=[];
+  for(int i=0;i<s;i++)
     {
-      print('your son  ${student[i]} is present at today');
+      String name=stdin.readLineSync()!;
+      sname.add(name);
+
+    }
+  for(int i=0;i<sname.length;i++)
+    {
+      print('your son  ${sname[i]} is present at today');
     }
 }
