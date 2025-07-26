302ea323413b7db750395ddf03f5303c7f80f940
miel2793 <mahmudsifat2793@gmail.com>
2025-07-26 23:02:53 +0600

    new commit added

--- bin/loop/for_loop_at_list.dart
+++ bin/loop/for_loop_at_list.dart
@@ -10,8 +10,8 @@ main()
        String name=stdin.readLineSync()!;
        wName.add(name);
      }
-   for(int i=0;i<wName.length;i++)
+   for(  var nulu in wName)
      {
-       print("Dear ${wName[i]} . You Are Absent Today  Without any  Permission .\nSo we Decided To Cut Your Todays Payment\nThank You ");
+       print("Dear ${nulu } . You Are Absent Today  Without any  Permission .\nSo we Decided To Cut Your Todays Payment\nThank You ");
      }
 }
