d15c768dd10b0854cfa2929f79b20864bbdcbd76
miel2793 <mahmudsifat2793@gmail.com>
2025-07-26 22:33:50 +0600

    new commit added

---
+++ bin/loop/for_loop_at_list.dart
@@ -0,0 +1,8 @@
+main()
+{
+  List <String> student =['Rahim','Karim','ovi','robin','Sulaiman','Anas'];
+  for(int i=0;i<student.length;i++)
+    {
+      print('your son  ${student[i]} is present at today');
+    }
+}
