29e049bfced2628655db41da83cf06a032c7a5b1
miel2793 <mahmudsifat2793@gmail.com>
2025-07-27 22:14:58 +0600

    new commit added

--- bin/Function/function_1.dart
+++ bin/Function/function_1.dart
@@ -1,31 +1,9 @@
-import 'dart:io';
-
-main() {
-  int a = int.parse(stdin.readLineSync()!);
-  int b = int.parse(stdin.readLineSync()!);
-
-  l(a, b);
+main()
+{
+  print(sum(4,8));
 }
-
-sum(int a, int b) {
-  int sum;
-  sum = a + b;
-  print(sum);
-  return sum;
-}
-
-sub(int a, int b) {
-  int sub;
-  sub = a - b;
-  print(sub);
-  return sub;
-}
-
-l(int a, int b) {
-  if (a == b) {
-    print('we are same ');
-  } else {
-    print('we are not ');
-  }
-  //return l;
+ sum(int a,int b)
+{
+  int s= a+b;
+  return s;
 }
