834b0e103d43fb9de9fccca191400d58a8b99569
miel2793 <mahmudsifat2793@gmail.com>
2025-08-02 22:43:40 +0600

    new commit added

---
+++ bin/oop/class.dart
@@ -0,0 +1,19 @@
+import'human.dart';
+main()
+{
+  student stu1=student();
+  /*stu1.name='Miel';
+  stu1.intake=52;
+  stu1.id=368;*/
+ // print(stu1.id);
+  human d1=human();
+  print(d1.eye);
+
+}
+class student
+{
+   String ? name='miel';
+   int? id=34;
+    int? intake=48;
+
+}
---
+++ bin/oop/human.dart
@@ -0,0 +1,8 @@
+  class human
+  {
+    String? name;
+    int leg=2;
+    int eye=2;
+    int nose=1;
+
+  }
