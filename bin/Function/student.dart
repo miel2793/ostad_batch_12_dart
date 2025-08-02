1dfaa30987331c24dcf63913a58c9dd524d25c21
miel2793 <mahmudsifat2793@gmail.com>
2025-08-02 22:20:45 +0600

    new commit added

--- bin/Function/student.dart
+++ bin/Function/student.dart
@@ -1,8 +1,85 @@
 main()
 {
-  student_1('Miel mahmud Sifat', 20234103368, 52,'CSe');
+student_9();
+
 }
-  student_1(String name,int id,int intake,String dept)
+  student_1()
   {
+    String name='Miel Mahmud Sifat';
+    int intake=52;
+    String dept='cse';
+    int id=20234103368;
     print('Name:$name\nS_ID:$id\nIntake:$intake\nDepartment:$dept');
   }
+student_2()
+{
+  String name='Miel Mahmud Sifat';
+  int intake=52;
+  String dept='cse';
+  int id=20234103368;
+  print('Name:$name\nS_ID:$id\nIntake:$intake\nDepartment:$dept');
+}
+student_3()
+{
+  String name='Miel Mahmud Sifat';
+  int intake=52;
+  String dept='cse';
+  int id=20234103368;
+  print('Name:$name\nS_ID:$id\nIntake:$intake\nDepartment:$dept');
+}
+student_4()
+{
+  String name='Miel Mahmud Sifat';
+  int intake=52;
+  String dept='cse';
+  int id=20234103368;
+  print('Name:$name\nS_ID:$id\nIntake:$intake\nDepartment:$dept');
+}
+student_5()
+{
+  String name='Miel Mahmud Sifat';
+  int intake=52;
+  String dept='cse';
+  int id=20234103368;
+  print('Name:$name\nS_ID:$id\nIntake:$intake\nDepartment:$dept');
+}
+student_6()
+{
+  String name='Rakibul ISlam ';
+  int intake=56;
+  String dept='Economics';
+  int id=39952840;
+  print('Name:$name\nS_ID:$id\nIntake:$intake\nDepartment:$dept');
+}
+student_7()
+{
+  String name='Asif Iqbal';
+  int intake=02;
+  String dept='BBA';
+  int id=202341078998;
+  print('Name:$name\nS_ID:$id\nIntake:$intake\nDepartment:$dept');
+}
+student_8()
+{
+  String name='Badhon Islam ';
+  int intake=42;
+  String dept='Civil';
+  int id=202341564;
+  print('Name:$name\nS_ID:$id\nIntake:$intake\nDepartment:$dept');
+}
+student_9()
+{
+  String name='Nzamul Hasan  Siam ';
+  int intake=51;
+  String dept='cse';
+  int id=20224103356;
+  print('Name:$name\nS_ID:$id\nIntake:$intake\nDepartment:$dept');
+}
+student_10()
+{
+  String name='Anas Ibne Belal';
+  int intake=52;
+  String dept='cse';
+  int id=20234103372;
+  print('Name:$name\nS_ID:$id\nIntake:$intake\nDepartment:$dept');
+}
