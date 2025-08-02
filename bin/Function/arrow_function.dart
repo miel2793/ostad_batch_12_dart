8167e7a00b9e061e02d03fc697b6f58e4926737c
miel2793 <mahmudsifat2793@gmail.com>
2025-08-02 22:09:12 +0600

    new commit added

---
+++ bin/Function/arrow_function.dart
@@ -0,0 +1,5 @@
+main()
+ {
+print(cagl(4,5));
+ }
+var cagl=(int a,int b)=> (a+b);
---
+++ bin/Function/student.dart
@@ -0,0 +1,8 @@
+main()
+{
+  student_1('Miel mahmud Sifat', 20234103368, 52,'CSe');
+}
+  student_1(String name,int id,int intake,String dept)
+  {
+    print('Name:$name\nS_ID:$id\nIntake:$intake\nDepartment:$dept');
+  }
