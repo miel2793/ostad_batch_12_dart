7d653e720108eed45bd42d4227e7b22852931f81
miel2793 <mahmudsifat2793@gmail.com>
2025-08-09 22:04:48 +0600

    new code added

---
+++ bin/oop/inheritance.dart
@@ -0,0 +1,18 @@
+ class bubt {
+
+
+   int teacher = 150;
+   int student = 1200;
+   String vc = ' Saifur  Rahman Dipu';
+ }
+  class intake extends  bubt
+  {
+
+
+  }
+  main()
+  {
+    intake vf= intake();
+    print(vf.teacher);
+    
+  }
