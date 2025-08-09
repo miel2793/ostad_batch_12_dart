6470c7e432b5f0d5fb30160ac9ab854169bc5ca3
miel2793 <mahmudsifat2793@gmail.com>
2025-08-09 21:31:58 +0600

    new code added

--- bin/oop/constructor.dart
+++ bin/oop/constructor.dart
@@ -1,14 +1,19 @@
 ///কোনো   প্রি রিকয়ের্ড  ফাইল দরকার হলে  এটা ব্যাবহার হয়
 //    Class এর নাম আর অই ক class এ  method   এর নাম সেইম  হবে।
+// for perameter   use this .
   class miel
 {
-    miel()
+
+   String name ;
+   int ID;
+   int intake;
+    miel(this.name,this.ID,this.intake)
     {
-      print(" hi ");
+     print("Name:$name\nIntake:$intake\nID:$ID");
     }
 }
 
  main()
  {
-   miel();
+   miel('Miel Mahmud Sifat ', 20234103368, 52);
  }
