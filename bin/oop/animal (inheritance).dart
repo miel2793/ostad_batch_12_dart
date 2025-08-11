75133f6ca1f16a9337fa8ecec0b3255b7b0fc789
miel2793 <mahmudsifat2793@gmail.com>
2025-08-11 11:44:27 +0600

    new code added

---
+++ bin/oop/animal (inheritance).dart
@@ -0,0 +1,19 @@
+class Animal
+{
+
+   sound()
+   {
+      print(" Animal Make sound ");
+   }
+}
+
+ class Dog  extends Animal
+ {
+
+
+ }
+ main()
+ {
+    Dog  dog = Dog();
+    dog.sound();
+ }
