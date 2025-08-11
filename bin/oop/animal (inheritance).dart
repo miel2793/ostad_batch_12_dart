33fcd0c504fb8dd52d726f33bc138d867ac5f041
miel2793 <mahmudsifat2793@gmail.com>
2025-08-11 11:48:41 +0600

    new code added

--- bin/oop/animal (inheritance).dart
+++ bin/oop/animal (inheritance).dart
@@ -10,10 +10,26 @@ class Animal
  class Dog  extends Animal
  {
 
+    eat()
+    {
+ print(' Dog can eat  Briany ');
+    }
 
  }
+  class cat
+  {
+     String?  Name;
+
+      cat(this.Name)
+      {
+        print('The Name Of my Cat is $Name');
+      }
+
+  }
  main()
  {
     Dog  dog = Dog();
     dog.sound();
+    dog.eat();
+    cat('Misat');
  }
